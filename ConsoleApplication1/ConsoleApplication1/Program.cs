32a70c96bd147d9c66d19fd53373a4042d248228
iamgopi <iamgopin>
2021-05-10 13:48:37 +0530

    deleteed all

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,12 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-               Console.WriteLine("ok");
-            Console.WriteLine("pp");
-            Console.WriteLine("ok");
-            Console.WriteLine("pp");
-               Console.WriteLine("ok");
-            Console.WriteLine("pp");
+              
         }
     }
 }
