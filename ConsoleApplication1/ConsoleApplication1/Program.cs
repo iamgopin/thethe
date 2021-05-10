8e891cd22bba9b65ea1032b87c79d12b4a6e8576
iamgopin <83899802+iamgopin@users.noreply.github.com>
2021-05-10 13:47:43 +0530

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,9 +11,6 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
                Console.WriteLine("ok");
-            Console.WriteLine("pp");   Console.WriteLine("ok");
-            Console.WriteLine("pp");
-               Console.WriteLine("ok");
             Console.WriteLine("pp");
             Console.WriteLine("ok");
             Console.WriteLine("pp");
