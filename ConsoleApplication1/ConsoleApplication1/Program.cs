e86f88ee5f6bae33dfae2ea60259b73617c9472f
iamgopin <83899802+iamgopin@users.noreply.github.com>
2021-05-10 13:44:41 +0530

    thid
    ok

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+               Console.WriteLine("ok");
+            Console.WriteLine("pp");
             Console.WriteLine("ok");
             Console.WriteLine("pp");
                Console.WriteLine("ok");
